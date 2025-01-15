9e0f36aa0bf7907e81f30fe844b823a16749b3b5
Michael Rhey Palaganas <70465004+SeeFlushFlush64@users.noreply.github.com>
2025-01-15 21:17:51 +0800

    added BasicOccurrencesAnagram

---
+++ BasicCountOccurrencesAnagram/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicCountOccurrencesAnagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "listeninletsentil";
+            Console.WriteLine(CountOccurrenceAnagram(myString));
+        }
+
+        public static int CountOccurrenceAnagram(string testString)
+        {
+            int numberOfOccurrences = 0;
+            Dictionary<char, int> targetFreq = new Dictionary<char, int>
+            {
+                { 'l', 1 }, { 'i', 1 }, { 's', 1 }, { 't', 1 }, { 'e', 1 }, { 'n', 1 }
+            };
+
+            Dictionary<char, int> windowFreq = new Dictionary<char, int>();
+            int left = 0;
+            int requiredLength = targetFreq.Count;
+
+            for (int right = 0; right < testString.Length; right++)
+            {
+                char currentChar = testString[right];
+                if (targetFreq.ContainsKey(currentChar))
+                {
+                    // Add current character to the window
+                    if (!windowFreq.ContainsKey(currentChar))
+                        windowFreq[currentChar] = 0;
+                    windowFreq[currentChar]++;
+
+                    // Check if the window matches the target frequency
+                    if (windowFreq[currentChar] == targetFreq[currentChar])
+                        requiredLength--;
+                }
+
+                // Shrink the window if it exceeds the required size
+                if (right - left + 1 > targetFreq.Count)
+                {
+                    char leftChar = testString[left];
+                    if (targetFreq.ContainsKey(leftChar))
+                    {
+                        if (windowFreq[leftChar] == targetFreq[leftChar])
+                            requiredLength++;
+
+                        windowFreq[leftChar]--;
+                    }
+                    left++;
+                }
+
+                // Check if the current window is a valid anagram
+                if (requiredLength == 0)
+                    numberOfOccurrences++;
+            }
+
+            return numberOfOccurrences;
+        }
+    }
+}
