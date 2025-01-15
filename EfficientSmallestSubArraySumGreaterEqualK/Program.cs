5dc3b366113adc725d80ab89ccc12260d3b81ce0
Michael Rhey Palaganas <70465004+SeeFlushFlush64@users.noreply.github.com>
2025-01-15 21:08:07 +0800

    corrected while loop inside the for loop to subtract while the sum is greater or equal to k

---
+++ EfficientSmallestSubArraySumGreaterEqualK/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace EfficientSmallestSubArraySumGreaterEqualK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 10, 5, 2, 7 };
+            int k = 15;
+            System.Console.WriteLine(EfficientSmallestSubArraySize(array, k));
+            
+        }
+        static int EfficientSmallestSubArraySize(int[] array, int k)
+        {
+            int left = 0;
+            int windowSum = 0;
+            int smallestSize = 0;
+
+            for (int right = 0; right < array.Length; right++)
+            {
+                windowSum += array[right];
+                while (windowSum >= k)
+                {
+                    smallestSize = right - left + 1;
+                    windowSum -= array[left];
+                    left++;
+                }
+            } 
+            return smallestSize;
+        }
+    }
+}
