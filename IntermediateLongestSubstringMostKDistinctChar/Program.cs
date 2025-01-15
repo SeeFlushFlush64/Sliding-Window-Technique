06b1e922ae667d5c0097054b715caa47091f9abd
Michael Rhey Palaganas <70465004+SeeFlushFlush64@users.noreply.github.com>
2025-01-15 23:44:17 +0800

    added IntermediateLongestSubstringMostKDistinctChar

---
+++ IntermediateLongestSubstringMostKDistinctChar/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace IntermediateLongestSubstringMostKDistinctChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string apps = "aabacbebebe";
+            int k = 3;
+            System.Console.WriteLine(LongestSubStringMostKDistinct(apps, k));
+        }
+        static string LongestSubStringMostKDistinct(string apps, int k)
+        {
+            string longestSubstring = "";
+            string currentLongest = "";
+            int left = 0;
+            int longestSize = 0;
+            HashSet<char> distinctChars = new HashSet<char>();
+            for (int right = 0; right < apps.Length; right++)
+            {
+                if (distinctChars.Count() <= k)
+                {
+                    if (distinctChars.Count().Equals(k) && !distinctChars.Contains(apps[right]))
+                    {
+                        char toBeRemoved = apps[left];
+                        currentLongest = apps.Substring(left, right - left);
+                        longestSubstring = currentLongest.Length > longestSubstring.Length ? currentLongest : longestSubstring;
+                        currentLongest = longestSubstring;
+                        while (currentLongest.Contains(toBeRemoved))
+                        {
+                            currentLongest = currentLongest.Remove(0, 1);
+                            left++;
+                        }
+                        distinctChars.Remove(toBeRemoved);  
+                    }
+                    else if (distinctChars.Count().Equals(k) && right.Equals(apps.Length - 1))
+                    {
+                        currentLongest = apps.Substring(left, right - left + 1);
+                        longestSubstring = currentLongest.Length > longestSubstring.Length ? currentLongest : longestSubstring;
+                    }
+                    if (!distinctChars.Contains(apps[right]))
+                    {
+                        distinctChars.Add(apps[right]);
+                    }
+                }
+            }
+            return longestSubstring;
+        }
+    }
+}
